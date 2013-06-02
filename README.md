1357c9279c621567653609638ef50b7197858613
Beau Charman <beau.charman@gmail.com>
2013-06-02 20:25:15 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# WordPress Custom User Roles
+
+> Easily create and manage custom user roles and their capabilities in WordPress.
+
+## Usage
+
+```php
+
+$Moderator = new LT3_Custom_User_Role('moderator', 'Moderator', array('read', 'edit_posts'));
+
+```
+
+More information about roles and capabilities
+
+http://codex.wordpress.org/Roles_and_Capabilities
---
+++ custom-user-role.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Custom User Role
+ * ========================================================================
+ * custom-user-role.php
+ * @version 0.1 | May 2nd 2013
+ * @author  Beau Charman | @beaucharman | http://www.beaucharman.me
+ * @link    TBA
+ * @license MIT license
+ *
+ * More information about roles and capabilities
+ * http://codex.wordpress.org/Roles_and_Capabilities
+ */
+
+/* ========================================================================
+   Custom User Roles
+   ======================================================================== */
+class LT3_Custom_User_Role
+{
+  protected $name;
+  protected $label;
+  protected $capabilities;
+  protected $restrictions;
+
+  /**
+   * Class constructor
+   * ========================================================================
+   * __construct()
+   * @param  {string} $name
+   * @param  {string} $label
+   * @param  {array}  $capabilities
+   * @param  {array}  $restrictions
+   * ======================================================================== */
+  function __construct($name, $label = null, $capabilities = array(), $restrictions = array())
+  {
+    /* Set class values */
+    $this->name         = $name;
+    $this->label        = ($label) ? $label : ucwords(str_replace('_', ' ', $name));
+    $this->capabilities = $capabilities;
+    $this->restrictions = $restrictions;
+    add_action('init', array(&$this, 'add_custom_user_roles'));
+  }
+
+  /**
+   * Add custom user roles
+   * ========================================================================
+   * add_custom_user_roles()
+   * ======================================================================== */
+  public function add_custom_user_roles()
+  {
+    add_role($this->name, $this->label);
+    $role =& get_role($this->name);
+    foreach ($this->capabilities as $cap)
+    {
+      $role->add_cap($cap);
+    }
+    foreach ($this->restrictions as $res)
+    {
+      $role->remove_cap($res);
+    }
+  }
+}
